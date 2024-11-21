dbf68aa0b6121f082860ed54db0b2844d02207d5
SuJi Mo <ahtnwl1004@gmail.com>
2024-11-22 00:03:50 +0900

    project setting

---
+++ src/test/java/ewha/capston/cockChat/CockChatApplicationTests.java
@@ -0,0 +1,13 @@
+package ewha.capston.cockChat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CockChatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
