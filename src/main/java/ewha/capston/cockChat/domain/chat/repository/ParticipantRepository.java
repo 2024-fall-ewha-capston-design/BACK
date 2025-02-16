825391df55f5d7297d339f294cd5acf6156ac485
SuJi Mo <ahtnwl1004@gmail.com>
2025-02-16 15:58:47 +0900

    refactor : 채팅 참가자 필드 수정

--- src/main/java/ewha/capston/cockChat/domain/chat/repository/ParticipantRepository.java
+++
@@ -1,9 +0,0 @@
-package ewha.capston.cockChat.domain.chat.repository;
-
-import ewha.capston.cockChat.domain.chat.domain.Participant;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-//@Repository
-public interface ParticipantRepository extends JpaRepository<Participant, Long> {
-}
