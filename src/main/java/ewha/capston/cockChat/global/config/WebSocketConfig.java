0afc288aef5ec77a52e14034cc9eaa6797b613f4
SuJi Mo <ahtnwl1004@gmail.com>
2025-03-11 21:36:41 +0900

    chore : config 수정

--- src/main/java/ewha/capston/cockChat/global/config/WebSocketConfig.java
+++ src/main/java/ewha/capston/cockChat/global/config/WebSocketConfig.java
@@ -36,7 +36,7 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
                                         )
 
                  */
-               //.withSockJS()
+               .withSockJS()
             ;
     }
 
