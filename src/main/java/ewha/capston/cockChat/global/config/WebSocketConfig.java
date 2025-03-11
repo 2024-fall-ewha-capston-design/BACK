9bf13a08090869f42532e71d95798a60d544fd5b
SuJi Mo <ahtnwl1004@gmail.com>
2025-03-11 19:35:14 +0900

    chore : config 수정

--- src/main/java/ewha/capston/cockChat/global/config/WebSocketConfig.java
+++ src/main/java/ewha/capston/cockChat/global/config/WebSocketConfig.java
@@ -4,12 +4,14 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.EnableWebSocket;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
 import org.springframework.web.socket.handler.WebSocketHandlerDecoratorFactory;
 
 @Configuration
+@EnableWebSocket
 @RequiredArgsConstructor
 @EnableWebSocketMessageBroker
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
@@ -20,16 +22,22 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
         registry.setApplicationDestinationPrefixes("/app"); // 클라이언트가 서버로 보낼 때 사용하는 prefix
     }
 
+
+
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint("/ws-chat")
                 .setAllowedOriginPatterns("*")
-               // .setAllowedOriginPatterns("http://localhost:3000", "http://localhost:8080",
-               //                           "http://localhost:63342", "https://chatcipe.o-r.kr"
-                                          //,"wss://chatcipe.o-r.kr"
-                  //                       )  // 특정 도메인 허용
-               // .withSockJS()
+                /*
+               .setAllowedOriginPatterns("http://localhost:3000", "http://localhost:8080",
+                                        "http://localhost:63342", "https://chatcipe.o-r.kr"
+                                          ,"wss://chatcipe.o-r.kr"
+                                        )
+
+                 */
+               //.withSockJS()
             ;
     }
+
 }
 
