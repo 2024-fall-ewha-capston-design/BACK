ac104a04ab456540596e7665268c3550a60aa372
SuJi Mo <ahtnwl1004@gmail.com>
2025-03-14 01:06:40 +0900

    chore : config 수정

--- src/main/java/ewha/capston/cockChat/global/config/SecurityConfig.java
+++ src/main/java/ewha/capston/cockChat/global/config/SecurityConfig.java
@@ -50,7 +50,7 @@ public class SecurityConfig {
  */
 
 
-/*
+
 @Configuration
 @RequiredArgsConstructor
 public class SecurityConfig {
@@ -87,42 +87,3 @@ public class SecurityConfig {
         return http.build();
     }
 }
-
- */
-
-import lombok.RequiredArgsConstructor;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-
-@Configuration
-@RequiredArgsConstructor
-public class SecurityConfig {
-    private final JwtTokenProvider jwtTokenProvider;
-
-    @Bean
-    public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
-    }
-
-    @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http.csrf(csrf -> csrf.disable())
-                .cors(cors -> cors.configurationSource(request -> new CorsConfiguration().applyPermitDefaultValues())) // CORS 설정 활성화
-                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-                .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/ws-chat/**").permitAll() // WebSocket 엔드포인트 허용
-                        .anyRequest().authenticated()
-                )
-                .addFilterBefore(new JwtAuthenticationFilter(jwtTokenProvider), UsernamePasswordAuthenticationFilter.class);
-
-        return http.build();
-    }
-
-}
-
